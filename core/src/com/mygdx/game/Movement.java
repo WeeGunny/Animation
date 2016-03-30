22750fb7ceaaa432729bd61dcc1f70277cd97085
Ashleigh <ashcstroud@hotmail.com>
2016-03-30 10:10:37 -0400

    Fixed up naming conventions

--- core/src/com/mygdx/game/Movement.java
+++ core/src/com/mygdx/game/Movement.java
@@ -10,68 +10,68 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Movement extends ApplicationAdapter {
-	private static final int    COLS = 4;
-	private static final int    ROWS = 4;
+	private static final int    nCols = 4;
+	private static final int    nRows = 4;
 
-	SpriteBatch batch;
-	Texture Sprite;
-	Texture BackGround;
-	TextureRegion[] frames;
-	TextureRegion CurrentFrame;
-	float SpriteX=0;
-	float SpriteY=0;
-	float SpriteSpeed=45f;
-	float Time=0f;
-	Animation animation;
+	SpriteBatch sbMain;
+	Texture txSprite;
+	Texture txBackground;
+	TextureRegion[] trFrames;
+	TextureRegion trCurrentFrame;
+	float fSpriteX=0;
+	float fSpriteY=0;
+	float fSpriteSpeed=45f;
+	float fTime=0f;
+	Animation aniMain;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		BackGround= new Texture(Gdx.files.internal("lostwoods2.jpg"));
-		Sprite = new Texture(Gdx.files.internal("CinderellaSpriteSheet.png"));
-		TextureRegion[][] tmp = TextureRegion.split(Sprite,Sprite.getWidth()/COLS,Sprite.getHeight()/ROWS);
-		frames = new TextureRegion[COLS * ROWS];
+		sbMain = new SpriteBatch();
+		txBackground= new Texture(Gdx.files.internal("lostwoods2.jpg"));
+		txSprite = new Texture(Gdx.files.internal("CinderellaSpriteSheet.png"));
+		TextureRegion[][] tmp = TextureRegion.split(txSprite,txSprite.getWidth()/nCols,txSprite.getHeight()/nRows);
+		trFrames = new TextureRegion[nCols * nRows];
 		int index = 0;
-		for (int i = 0; i < ROWS; i++) {
-			for (int j = 0; j < COLS; j++) {
-				frames[index++] = tmp[i][j];
+		for (int i = 0; i < nRows; i++) {
+			for (int j = 0; j < nCols; j++) {
+				trFrames[index++] = tmp[i][j];
 			}
 		}
-		animation= new Animation(1f,frames);
+		aniMain= new Animation(1f,trFrames);
 	}
 
 	@Override
 	public void render () {
-		if(Time<4){
-		Time += Gdx.graphics.getDeltaTime();}
+		if(fTime<4){
+		fTime += Gdx.graphics.getDeltaTime();}
 		else{
-			Time=0;
+			fTime=0;
 		}
 
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		CurrentFrame = animation.getKeyFrame(0);
+		trCurrentFrame = aniMain.getKeyFrame(0);
 
 		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT)) {
-			SpriteX -= Gdx.graphics.getDeltaTime() * SpriteSpeed;
-			CurrentFrame = animation.getKeyFrame(4+Time);
+			fSpriteX -= Gdx.graphics.getDeltaTime() * fSpriteSpeed;
+			trCurrentFrame = aniMain.getKeyFrame(4+fTime);
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT)) {
-			SpriteX += Gdx.graphics.getDeltaTime() * SpriteSpeed;
-			CurrentFrame = animation.getKeyFrame(8+Time);
+			fSpriteX += Gdx.graphics.getDeltaTime() * fSpriteSpeed;
+			trCurrentFrame = aniMain.getKeyFrame(8+fTime);
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_UP)) {
-			SpriteY += Gdx.graphics.getDeltaTime() * SpriteSpeed;
-			CurrentFrame = animation.getKeyFrame(12+Time);
+			fSpriteY += Gdx.graphics.getDeltaTime() * fSpriteSpeed;
+			trCurrentFrame = aniMain.getKeyFrame(12+fTime);
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.DPAD_DOWN)) {
-			SpriteY -= Gdx.graphics.getDeltaTime() * SpriteSpeed;
-			CurrentFrame = animation.getKeyFrame(0+Time);
+			fSpriteY -= Gdx.graphics.getDeltaTime() * fSpriteSpeed;
+			trCurrentFrame = aniMain.getKeyFrame(0+fTime);
 		}
 
-		batch.begin();
-		batch.draw(BackGround,0,0);
-		batch.draw(CurrentFrame,(int)SpriteX, (int)SpriteY);
-		batch.end();
+		sbMain.begin();
+		sbMain.draw(txBackground,0,0);
+		sbMain.draw(trCurrentFrame,(int)fSpriteX, (int)fSpriteY);
+		sbMain.end();
 	}
 }
